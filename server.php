4edb8fb9c6373a824dfd703da356487e2bcb2166
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-09 17:41:53 +0200

    [UPD] updated server.php

--- server.php
+++ server.php
@@ -1,13 +1,27 @@
 <?php
-    $todolist = [
-        "HTML",
-        "CSS",
-        "Responsive design",
-        "JavaScript",
-        "PHP"
-    ];
+    // Controlla se il file esiste e carica il contenuto, altrimenti imposta una lista vuota
+    if (file_exists('./data/todo-list.json')) {
+        $string = file_get_contents('./data/todo-list.json');
+        $todolist = json_decode($string, true); // Decodifica come array
+    } else {
+        $todolist = [];
+    }
+
+    if(isset($_POST['addTodo'])){
+        $todo_item = $_POST['addTodo']; // Uso corretto di $_POST
+        array_push($todolist, $todo_item);
+
+        file_put_contents('./data/todo-list.json', json_encode($todolist));
+    }
+
+    if(isset($_POST['deleteTodo'])){
+        $index = $_POST['deleteTodo'];
+        
+        // Rimuove l'elemento dall'array
+        array_splice($todolist, $index, 1);
+        file_put_contents('./data/todo-list.json', json_encode($todolist));
+    }
 
     header('Content-type: application/json');
     echo json_encode($todolist);
-
 ?>
